4a2257c49e5c2d9e0af79a838e71d3b7a1ae78f1
Mike Merin <mikemerinweather@gmail.com>
2017-10-12 12:43:56 -0400

    dictionary constants, script for scraping

---
+++ src/letters.rb
@@ -0,0 +1,10 @@
+open("/Users/flatironschool/Downloads/english3sorted.txt", "w") { |x| x << eval(File.open("/Users/flatironschool/Downloads/english3.txt").first).sort }
+
+dictionary = eval(File.open("/Users/flatironschool/Downloads/english3sorted.txt").first)
+("a".."z").to_a.each do |letter|
+  File.open("/Users/flatironschool/regex/src/dictionary.js", "a") do |x|
+    words, dictionary = dictionary.partition { |word| word.match(/^#{letter}/) }
+    puts "#{letter} has #{words.size} words added"
+    x << "export const #{letter} = #{words}\n\n\n\n\n\n"
+  end
+end
